9d4b7111b03dd02ac349b0d5c1824ccf39383696
xanderle <x.marks.the.spot.xd@gmail.com>
2017-05-12 19:13:58 +0800

    Added graph functionality

--- disease/model.py
+++ disease/model.py
@@ -3,8 +3,15 @@ from random import random
 from mesa import Model
 from mesa.time import SimultaneousActivation
 from mesa.space import Grid
-
+from mesa.datacollection import DataCollector
 from disease.cell import Cell
+
+def calcNoInfected(model):
+    count = 0
+    for infected in model.schedule.cell:
+        if infected.status == 0:
+            count+=1
+    return count
 
 
 class Disease(Model):
@@ -16,7 +23,9 @@ class Disease(Model):
         '''
         Create a new playing area of (height, width) cells.
         '''
-
+        self.noInfected = 0
+        self.noVaccinated = 0
+        self.noAlive = 0
         # Set up the grid and schedule.
 
         # Use SimultaneousActivation which simulates all the cells
@@ -27,19 +36,26 @@ class Disease(Model):
 
         # Use a simple grid, where edges wrap around.
         self.grid = Grid(height, width, torus=True)
-
+        self.datacollector = DataCollector(
+            {"Infected": lambda m: self.count_type(m, 0),
+             "Vaccinated": lambda m: self.count_type(m, 3),
+             "Alive": lambda m: self.count_type(m, 1)})
         # Place a cell at each location, with some initialized to
         # ALIVE and some to DEAD.
         for (contents, x, y) in self.grid.coord_iter():
             cell = Cell((x, y), self)
             if random() < .1:
                 cell.state = cell.INFECTED
-            elif random() < .9:
+                self.noInfected+=1
+            elif random() < .5:
                 cell.state = cell.VACCINATED
+                self.noVaccinated +=1
             else:
                 cell.state = cell.ALIVE
+                self.noAlive+=1
             self.grid.place_agent(cell, (x, y))
             self.schedule.add(cell)
+
         self.running = True
 
     def step(self):
@@ -47,3 +63,16 @@ class Disease(Model):
         Have the scheduler advance each cell by one step
         '''
         self.schedule.step()
+        self.datacollector.collect(self)
+
+    @staticmethod
+    def count_type(model,condition):
+        """
+        Helper method to count trees in a given condition in a given model.
+        """
+
+        count = 0
+        for cell in model.schedule.agents:
+            if cell.state == condition:
+                count += 1
+        return count
--- disease/server.py
+++ disease/server.py
@@ -1,4 +1,4 @@
-from mesa.visualization.modules import CanvasGrid
+from mesa.visualization.modules import CanvasGrid, ChartModule
 from mesa.visualization.ModularVisualization import ModularServer
 
 from disease.portrayal import portrayCell
@@ -6,7 +6,11 @@ from disease.model import Disease
 
 
 # Make a world that is 50x50, on a 250x250 display.
-canvas_element = CanvasGrid(portrayCell, 50, 50, 250, 250)
+canvas_element = CanvasGrid(portrayCell, 100, 100, 600, 600)
 
-server = ModularServer(Disease, [canvas_element], "Herd Immunity",
-                       50, 50)
+tree_chart = ChartModule([{"Label": "Alive", "Color": "green"},
+                          {"Label": "Infected", "Color": "red"},
+                          {"Label": "Vaccinated", "Color": "blue"}])
+
+server = ModularServer(Disease, [canvas_element,tree_chart], "Herd Immunity",
+                       100, 100)
